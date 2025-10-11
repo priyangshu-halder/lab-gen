85c35cf23efa13eab403c3b697ae46746933d402
priyangshu-halder <contact.priyangshu.work@gmail.com>
2025-10-12 00:33:17 +0530

    Info Field Added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Header } from './components/Header/Header.tsx'
+import { LabInfo } from './components/LabInfo/LabInfo.tsx'
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header/>
+      <LabInfo/>
     </>
   )
 }
--- client/src/components/LabInfo/LabInfo.tsx
+++ client/src/components/LabInfo/LabInfo.tsx
@@ -1,3 +1,28 @@
-export function LabInfo(){
-    return true
+import { Input } from "../ui/input.tsx"
+import { Label } from "../ui/label.tsx"
+export function LabInfo() {
+  return (
+    <div className="space-y-6">
+      <h4 className="text-lg font-semibold">Laboratory Information</h4>
+      <div className="grid w-full max-w-sm items-center gap-3">
+        <Label htmlFor="lab-name">Laboratory Name:</Label>
+        <Input type="text" id="lab-name" placeholder="Enter Laboratory Name" />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-3">
+        <Label htmlFor="address">Address</Label>
+        <Input type="text" id="address" placeholder="Enter Address" />
+      </div>
+
+      <div className="grid grid-cols-2 gap-3 w-full max-w-sm">
+        <div className="flex flex-col">
+          <Label htmlFor="from-date">From Date</Label>
+          <Input type="date" id="from-date" />
+        </div>
+        <div className="flex flex-col">
+          <Label htmlFor="to-date">To Date</Label>
+          <Input type="date" id="to-date" />
+        </div>
+      </div>
+    </div>
+  )
 }
